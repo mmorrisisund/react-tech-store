4649e5749e3d1cd5b79191cd3a84f4e6914c7d6f
MiMo <michael.a.morris.public@gmail.com>
2020-08-06 22:04:21 -0500

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-export default function App() {
-  return <h1>hello from app component</h1>;
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Error from './pages/Error'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+
+import Header from './components/Header'
+
+export default function App () {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/products/:id' children={<ProductDetails />} />
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
